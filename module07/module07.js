8b8a0441c8054f611ee2eca8f21d54efefb8ae7a
faria <fnu.faria@bellevuecollege.edu>
2019-05-18 14:47:36 -0700

    new color

--- module07/module07.js
+++ module07/module07.js
@@ -8,12 +8,10 @@ function Hotel(name, rooms, booked) {
   };
 }
 
-
 // Create two hotel objects
 var quayHotel = new Hotel('Quay', 40, 25);
 var parkHotel = new Hotel('Park', 120, 77);
-
-
+var sunsetHotel = new Hotel('Sunset', 86, 10);
 // Update the HTML for the page
 var details1 = quayHotel.name + ' rooms: ';
     details1 += quayHotel.checkAvailability();
@@ -24,6 +22,11 @@ var details2 = parkHotel.name + ' rooms: ';
     details2 += parkHotel.checkAvailability();
 var elHotel2 = document.getElementById('hotel2');
 elHotel2.textContent = details2;
+
+var details3 = sunsetHotel.name + ' rooms: ';
+    details3 += sunsetHotel.checkAvailability();
+var elHotel3 = document.getElementById('hotel3');
+elHotel3.textContent = details3;
 
 /*
 NOTE: textContent does not work in IE8 or earlier
