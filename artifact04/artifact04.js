a875a3dd9d6ca99ac24fb3bf28f45e6f464d3ecd
Faria <faria.kazimi@gmail.com>
2019-06-01 12:34:09 -0700

    center

--- artifact04/artifact04.js
+++ artifact04/artifact04.js
@@ -1,45 +1,118 @@
- function ValidateFrom() {
-   var validUsername = false;
-   var validpassword = false;
-   var validFirstName = false;
-   var validphone = fales;
+function inputAlphabet(inputtext){
+var alphaExp = /^[a-zA-Z]+$/;
+if(inputtext.match(alphaExp)){
+return true;
+}else{
+return false;
+}
+}
+function textNumeric(inputtext){
+var numericExpression = /^[0-9]+$/;
+if(inputtext.match(numericExpression)){
+return true;
+}else{
+return false;
+}
+}
 
-   var letters = /^[A-Za-z]+$/;
-   var numbers = /^[0-9]+$/;
-   
-   var errorMessages = "";  //All the error Messages are going to stay in this variable
-   /***********VALIDATES USERNAEM ******** */
-   //Required filed
-   //This syntax is using name-of-form.name-of-filed.value
-   //you can also use document .getElementById("id-of-filed").value
-
-   /***********VALIDATES USERNAEM ******** */
-   //Required. Maximum 12 characters.
-   if (mycontact.username.value.lenght > 12 || mycontact.username.value===null || mycontact.username.value==="")
-   errorMessages += "<p>The username must be less than 12 characters and is required</P>";
-   else
-   validUsername =true;
-
-   //console.log(validUsername);
-      /***********VALIDATES PASSWORD ******** */
-      if (mycontact.password.value==null || mycontact.password.value=== "" || mycontact.password.value.lenght >7)
-      errorMessages += "<p>The password must be less than 7 characters and is required</P>";
-      else
-      validUserpassword =true;
-
-      /***********VALIDATES FIRSTNAME ******** */
-      if (mycontact.firstname.value==null || mycontact.firstname.value=== "" || mycontact.firstname.value.lenght >20 || !ycontact.firstname.value.match(letters))
-      errorMessages += "<p>The firstname must be less than 20 characters and is required. only letters and numbers are accepted</P>";
-      else
-      validFirstName =true;
-
-      /***********VALIDATES PHONENUMBER ******** */
-      if (mycontact.Phone.value==null || mycontact.Phone.value=== "" || mycontact.Phone.value.lenght >20 || !ycontact.Phone.value.match(numbers))
-      errorMessages += "<p>The phone number must be less than 15 characters and is required. only numbers are accepted</P>";
-      else
-      validphone =true;
-
-      document.getElementById("errorMessages").innerHTML = errorMessages;
-      //make sure you return all the boolean variable that are checking each filed
-      return (validUsername && validpassword && validFirstName && validphone) ;
+function validateFirstName() {
+    var x = document.getElementById('firstName').value;
+    if (!inputAlphabet(x) || x.length>20) {
+        alert("Not a valid first name");
+        return false;
     }
+  return true;
+}
+function validateLastName() {
+    var x = document.getElementById('lastName').value;
+    if (!inputAlphabet(x) || x.length>50) {
+        alert("Not a valid last name");
+        return false;
+    }
+  return true;
+}
+
+function validateEmail() {
+    var x = document.getElementById('email').value;
+    var atpos = x.indexOf("@");
+    var dotpos = x.lastIndexOf(".");
+    if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=x.length) {
+        alert("Not a valid e-mail address");
+        return false;
+    }
+  return true;
+}
+function validatePhone() {
+    var x = document.getElementById('phone').value;
+    if (!textNumeric(x) || x.length>15) {
+        alert("Not a valid phone number");
+        return false;
+    }
+  return true;
+}
+
+function validateUsername() {
+    var x = document.getElementById('username').value;
+    if (x.length>12 || x.length===0) {
+        alert("Not a valid username");
+        return false;
+    }
+  return true;
+}
+
+function validatePassword() {
+    var x = document.getElementById('password').value;
+    if (x.length>7 || x.length===0) {
+        alert("Not a valid password");
+        return false;
+    }
+  return true;
+}
+function validateAddress() {
+    var x = document.getElementById('address').value;
+    if (x.length===0) {
+        alert("Not a valid address");
+        return false;
+    }
+  return true;
+}
+function validateCity() {
+    var x = document.getElementById('city').value;
+    if (x.length===0) {
+        alert("Not a valid city");
+        return false;
+    }
+  return true;
+}
+function validateCountry() {
+    var x = document.getElementById('country').value;
+    if (x.length===0) {
+        alert("Not a valid country");
+        return false;
+    }
+  return true;
+}
+
+function validateZip() {
+  var x = document.getElementById('zip').value;
+  var y = document.getElementById('country').value;
+    if (y.length==2){
+      if(x.length>5 || x.length===0){
+        alert("Not a valid Zip");
+        return false;
+      }else{
+        return true;
+      }
+    }
+  return true;
+}
+function validateForm() {
+  if (!validateFirstName() || !validateLastName() || !validateEmail() || !validatePhone() || !validateUsername() || !validatePassword() || !validateAddress() || !validateCity() || !validateCountry() || !validateZip()) {
+    alert('Please fix errors to submit.');
+    return false;
+  }
+  else {
+    alert('Thank you.');
+    return true;
+  }
+}
--- artifact05/artifact05.js
+++ artifact05/artifact05.js
@@ -1,10 +1,12 @@
-var myImages =["https://www.imms.org/images/dolphin_presentation/dolphin1.jpg", "http://cdn3.theeventchronicle.com/wp-content/uploads/2014/09/dolphin-intelligence.jpg","http://www.liveanimalslist.com/mammals/images/dolphin-in-water.jpg","http://i1151.photobucket.com/albums/o632/bowietvc15/Bowietvc15-2/DolphinsWallpaper2.jpg","http://i1151.photobucket.com/albums/o632/bowietvc15/funny%20gifs/animal-art-cute-dolphin-nature-Favimcom-311780.jpg"];
-
-
-var captionImages =["Laugh","Love","Lunch","Happiness","Family"];
+var myImages = ["https://www.smartertravel.com/uploads/2017/08/Grand-Canyon-%E2%80%93-hero-1400x500.jpg",
+               "https://assets.simpleviewinc.com/simpleview/image/upload/c_fill,g_north_west,h_339,q_75,w_610/v1/clients/roanoke/VBR_Sunset_68aa8771-78aa-4cab-a4e6-2035319710d1.jpg",
+               "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/vibrant-chicago-skyline-sunset-jasmin-omerovic.jpg",
+               "https://media.cntraveler.com/photos/5894b2d829a6a9db132b077c/master/pass/GettyImages-118998690.jpg",
+               "https://www.exploregeorgia.org/sites/default/files/styles/slideshow_large/public/legacy_images/forsyth-park-fountain-1-1512424285.jpg?itok=6qo2dtMH"];
+var captionImages =
+["The Grand Canyon!","The Blue Ridge Parkway!", "Chicago’s Skyline!", "Haleakala National Park!","Savannah, Georgia!"];
 
  var index=0;
-
  function updateImage(){
  document.getElementById("slideshow").src = myImages[index];
  document.getElementById("slideshow").alt= captionImages[index];
@@ -33,4 +35,9 @@ var nextButton = document.getElementById("next");
 var previousButton = document.getElementById("previous");
 
 previousButton.addEventListener("click",back,false);
-nextButton.addEventListener("click",next,false); 
+nextButton.addEventListener("click",next,false);
+function autoSlide(){
+if (document.getElementById("auto").checked)
+ next();
+}
+setInterval(autoSlide,3000); // Next
